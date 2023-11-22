9e94037d7fb0c3f0e14e0dc57c3eb426f6026873
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-11-22 11:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById('openModal');
+  var closeBtn = document.querySelector('.close-modal');
+
+  function openModal() {
+    modal.style.display = 'block';
+  }
+
+  function closeModal() {
+    modal.style.display = 'none';
+  }
+
+  btn.addEventListener('click', openModal);
+  closeBtn.addEventListener('click', closeModal);
+
+  window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+});
